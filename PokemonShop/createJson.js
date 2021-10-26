9f71c631c45a9869fdaf5b4f7777c17e2e6a6d4a
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-10-26 21:25:53 +0900

    Add: import array json

---
+++ PokemonShop/createJson.js
@@ -0,0 +1,80 @@
+const obj = [
+  {
+    id: "0",
+    name: "Holly",
+    picture: "image/Blacky.png",
+    age: "5",
+    location: "New York",
+    price: "3ETH",
+  },
+
+  {
+    id: "1",
+    name: "Hope",
+    picture: "image/Bulbasaur.png",
+    age: "8",
+    location: "Chicago",
+    price: "2ETH",
+  },
+
+  {
+    id: "2",
+    name: "Love",
+    picture: "image/Charmander.jpg",
+    age: "4",
+    location: "Minnesota",
+    price: "1ETH",
+  },
+  {
+    id: "3",
+    name: "Future",
+    picture: "image/Eevee.png",
+    age: "2",
+    location: "Kentucky",
+    price: "3ETH",
+  },
+  {
+    id: "4",
+    name: "Heebong",
+    picture: "image/Glaceon.png",
+    age: "7",
+    location: "Seoul",
+    price: "2ETH",
+  },
+  {
+    id: "5",
+    name: "Coco",
+    picture: "image/Metamon.png",
+    age: "3",
+    location: "Pennsylvania",
+    price: "1ETH",
+  },
+  {
+    id: "6",
+    name: "Alpha",
+    picture: "image/Pukrin.png",
+    age: "7",
+    location: "Michigan",
+    price: "2ETH",
+  },
+  {
+    id: "7",
+    name: "Beta",
+    picture: "image/Purin.png",
+    age: "5",
+    location: "Los Angeles",
+    price: "3ETH",
+  },
+  {
+    id: "8",
+    name: "Niko",
+    picture: "image/Snorlax.png",
+    age: "4",
+    location: "Tokyo",
+    price: "2ETH",
+  },
+];
+
+const json = JSON.stringify(obj);
+
+console.log(json);
